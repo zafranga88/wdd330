7a4e70a744de2ee67919b04154269e38f1c8c9cc
zafranga88 <zfrancogabriel@hotmail.com>
2025-09-10 15:52:29 -0300

    yup

--- src/js/product.js
+++ src/js/product.js
@@ -8,13 +8,3 @@ const productID = getParam("product");
 const product = new ProductDetails(productID, dataSource);
 product.init();
 
-// // add to cart button event handler
-// async function addToCartHandler(e) {
-//   const product = await dataSource.findProductById(e.target.dataset.id);
-//   addProductToCart(product);
-// }
-
-// // add listener to Add to Cart button
-// document
-//   .getElementById("addToCart")
-//   .addEventListener("click", addToCartHandler);
